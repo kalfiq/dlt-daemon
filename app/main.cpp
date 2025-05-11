ca6e5005da90f6760f0f9034824eaf50303dde98
Haikal <haikalbinrafiq@gmail.com>
2025-05-11 13:42:37 +0800

    feat: modify app to simulate DLT log inflow

--- app/main.cpp
+++ app/main.cpp
@@ -1,20 +1,20 @@
 #include <dlt/dlt.h>
+#include <sys/time.h>
 
 DLT_DECLARE_CONTEXT(ctx); /* declare context */
 
 int main()
 {
 	DLT_REGISTER_APP("TAPP", "Test Application for Logging");
-
 	DLT_REGISTER_CONTEXT(ctx, "TES1", "Test Context for Logging");
-
-	/* … */
-
-	DLT_LOG(ctx, DLT_LOG_ERROR, DLT_CSTRING("This is an error"));
-
-	/* … */
+	
+	while (true) {
+		DLT_LOG(ctx, DLT_LOG_INFO, DLT_CSTRING("This is a test log"));
+		sleep(10);
+	}
 
 	DLT_UNREGISTER_CONTEXT(ctx);
 	DLT_UNREGISTER_APP();
+
 	return 0;
 }
